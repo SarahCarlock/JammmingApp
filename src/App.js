2044db920fd25263b11c7844666e720cebd9f2bc
Sarah Carlock <sarah.carlock86@gmail.com>
2023-05-25 09:12:41 -0500

    download new create-react-app and swap in Jammming

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
 
 function App() {
   return (
@@ -8,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test the Jam
-          Well
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
